f3ca12a5ec6e1c4ed112a369238e8f61b4b2f2d0
Arlo Belshee <arlo.belshee@live.com>
2012-04-29 00:39:05 -0700

    Prepping for another release.

--- Simulated.FileSystem/Properties/AssemblyInfo.cs
+++ Simulated.FileSystem/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Simulated.FileSystem")]
 [assembly: AssemblyDescription("This package provides a transactional view of a file system and an in-memory simulator for that view. This makes it easy to test code that interacts with the file system.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Arlo Belshee")]
+[assembly: AssemblyCompany("arlobelshee")]
 [assembly: AssemblyProduct("Simulated.FileSystem")]
 [assembly: AssemblyCopyright("Copyright © Arlo Belshee 2011")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
