4a1c68e2e80e58e68c2f205888ef8798f35a305d
Arlo Belshee <arlo.belshee@live.com>
2014-01-22 19:55:18 -0800

    Now testing for identical behavior at the Disk level, rather that at the whole lib. We can encapsulate the variation.

---
+++ Simulated.FileSystem.Tests/EncapsulateDifferencesBetweenDiskAndMemory/DirectoryAndFileOperations.cs
@@ -0,0 +1,95 @@
+﻿// SimulatableAPI
+// File: DirectoryAndFileOperations.cs
+// 
+// Copyright 2011, Arlo Belshee. All rights reserved. See LICENSE.txt for usage.
+
+using System;
+using FluentAssertions;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Simulated.Tests.zzTestHelpers;
+using Simulated._Fs;
+
+namespace Simulated.Tests.EncapsulateDifferencesBetweenDiskAndMemory
+{
+	[TestFixture]
+	public abstract class DirectoryAndFileOperations
+	{
+		private const string ArbitraryFileContents = "contents";
+		private FsPath _baseFolder;
+		private _IFsDisk _testSubject;
+
+		[Test]
+		public void NewDirectoryShouldExistWhenCreated()
+		{
+			var newPath = _baseFolder/"sub";
+			_testSubject.DirExists(newPath)
+				.Should()
+				.BeFalse();
+			_testSubject.CreateDir(newPath);
+			_testSubject.DirExists(newPath)
+				.Should()
+				.BeTrue();
+		}
+
+		[Test]
+		public void CreatingDirectoryWhichExistsShouldNoop()
+		{
+			var newPath = _baseFolder/"sub";
+			var filePath = newPath/"file.txt";
+			_testSubject.CreateDir(newPath);
+			_testSubject.Overwrite(filePath, ArbitraryFileContents);
+			_testSubject.CreateDir(newPath);
+			_testSubject.DirExists(newPath)
+				.Should()
+				.BeTrue();
+			_testSubject.ShouldBeFile(filePath, ArbitraryFileContents);
+		}
+
+		[Test]
+		public void DeletedDirectoryShouldNotExist()
+		{
+			var newPath = _baseFolder / "sub";
+			_testSubject.CreateDir(newPath);
+			_testSubject.DirExists(newPath)
+				.Should()
+				.BeTrue();
+			_testSubject.DeleteDir(newPath);
+			_testSubject.DirExists(newPath)
+				.Should()
+				.BeFalse();
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			_testSubject = _MakeTestSubject();
+			var runName = "TestRun-" + Guid.NewGuid()
+				.ToString("N");
+			_baseFolder = FsPath.TempFolder/runName;
+			_testSubject.CreateDir(_baseFolder);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			_testSubject.DeleteDir(_baseFolder);
+		}
+
+		[NotNull]
+		internal abstract _IFsDisk _MakeTestSubject();
+	}
+
+	public class DirectoryAndFileOperationsDiskFs : DirectoryAndFileOperations {
+		internal override _IFsDisk _MakeTestSubject()
+		{
+			return new _DiskReal();
+		}
+	}
+	public class DirectoryAndFileOperationsMemoryFs : DirectoryAndFileOperations {
+		internal override _IFsDisk _MakeTestSubject()
+		{
+			return new _DiskSimulated();
+		}
+	}
+}
--- Simulated.FileSystem.Tests/zzTestHelpers/_TestHelperExtensions.cs
+++ Simulated.FileSystem.Tests/zzTestHelpers/_TestHelperExtensions.cs
@@ -11,13 +11,13 @@ namespace Simulated.Tests.zzTestHelpers
 {
 	internal static class _TestHelperExtensions
 	{
-		public static void ShouldExist(this FsDirectory dir)
+		public static void ShouldExist([NotNull] this FsDirectory dir)
 		{
 			dir.Exists.Should()
 				.BeTrue();
 		}
 
-		public static void ShouldContain([NotNull] this FsFile file, string contents)
+		public static void ShouldContain([NotNull] this FsFile file, [NotNull] string contents)
 		{
 			file.Exists.Should()
 				.BeTrue();
@@ -26,15 +26,42 @@ namespace Simulated.Tests.zzTestHelpers
 				.Be(contents);
 		}
 
-		public static void ShouldNotExist(this FsDirectory dir)
+		public static void ShouldNotExist([NotNull] this FsDirectory dir)
 		{
 			dir.Exists.Should()
 				.BeFalse();
 		}
 
-		public static void ShouldNotExist(this FsFile file)
+		public static void ShouldNotExist([NotNull] this FsFile file)
 		{
 			file.Exists.Should()
+				.BeFalse();
+		}
+
+		public static void ShouldBeDir([NotNull] this _IFsDisk disk, [NotNull] FsPath dir)
+		{
+			disk.DirExists(dir)
+				.Should()
+				.BeTrue();
+		}
+
+		public static void ShouldBeFile([NotNull] this _IFsDisk disk, [NotNull] FsPath file, [NotNull] string contents)
+		{
+			disk.FileExists(file)
+				.Should()
+				.BeTrue();
+			disk.TextContents(file)
+				.Should()
+				.Be(contents);
+		}
+
+		public static void ShouldNotExist([NotNull] this _IFsDisk disk, [NotNull] FsPath path)
+		{
+			disk.DirExists(path)
+				.Should()
+				.BeFalse();
+			disk.FileExists(path)
+				.Should()
 				.BeFalse();
 		}
 	}
