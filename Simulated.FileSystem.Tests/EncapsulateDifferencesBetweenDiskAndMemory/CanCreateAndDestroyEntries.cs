d9e72f29c153edebb89ea11243e5f895cd885a36
Arlo Belshee <arlo.belshee@live.com>
2014-02-04 08:33:43 -0800

    Finished splitting the low-level tests by purpose.

--- Simulated.FileSystem.Tests/EncapsulateDifferencesBetweenDiskAndMemory/CanCreateAndDestroyEntries.cs
+++ Simulated.FileSystem.Tests/EncapsulateDifferencesBetweenDiskAndMemory/CanCreateAndDestroyEntries.cs
@@ -1,12 +1,10 @@
 ﻿// SimulatableAPI
-// File: DirectoryAndFileOperations.cs
+// File: CanCreateAndDestroyEntries.cs
 // 
 // Copyright 2011, Arlo Belshee. All rights reserved. See LICENSE.txt for usage.
 
 using System;
-using System.Linq;
 using FluentAssertions;
-using JetBrains.Annotations;
 using NUnit.Framework;
 using Simulated.Tests.zzTestHelpers;
 using Simulated._Fs;
@@ -14,7 +12,7 @@ using Simulated._Fs;
 namespace Simulated.Tests.EncapsulateDifferencesBetweenDiskAndMemory
 {
 	[TestFixture]
-	public abstract class DirectoryAndFileOperations : DiskTestBase
+	public abstract class CanCreateAndDestroyEntries : DiskTestBase
 	{
 		[Test]
 		public void NewDirectoryShouldExistWhenCreated()
@@ -53,21 +51,6 @@ namespace Simulated.Tests.EncapsulateDifferencesBetweenDiskAndMemory
 				.Should()
 				.BeTrue();
 			TestSubject.ShouldBeFile(filePath, ArbitraryFileContents);
-		}
-
-		[Test]
-		[TestCase("matches.*", new[] {"matches.txt", "matches.jpg"})]
-		[TestCase("*.*", new[] {"matches.txt", "matches.jpg", "no_match.txt"})]
-		[TestCase("*.txt", new[] {"matches.txt", "no_match.txt"})]
-		[TestCase("matches.txt", new[] {"matches.txt"})]
-		public void FileMatchingShouldMatchStarPatterns([NotNull] string searchPattern, [NotNull] string[] expectedMatches)
-		{
-			TestSubject.Overwrite(BaseFolder/"matches.txt", ArbitraryFileContents);
-			TestSubject.Overwrite(BaseFolder/"matches.jpg", ArbitraryFileContents);
-			TestSubject.Overwrite(BaseFolder/"no_match.txt", ArbitraryFileContents);
-			TestSubject.FindFiles(BaseFolder, searchPattern)
-				.Should()
-				.BeEquivalentTo(expectedMatches.Select(m => BaseFolder/m));
 		}
 
 		[Test]
@@ -127,7 +110,7 @@ namespace Simulated.Tests.EncapsulateDifferencesBetweenDiskAndMemory
 		}
 	}
 
-	public class DirectoryAndFileOperationsDiskFs : DirectoryAndFileOperations
+	public class CanCreateAndDestroyEntriesDiskFs : CanCreateAndDestroyEntries
 	{
 		internal override _IFsDisk _MakeTestSubject()
 		{
@@ -135,7 +118,7 @@ namespace Simulated.Tests.EncapsulateDifferencesBetweenDiskAndMemory
 		}
 	}
 
-	public class DirectoryAndFileOperationsMemoryFs : DirectoryAndFileOperations
+	public class CanCreateAndDestroyEntriesMemoryFs : CanCreateAndDestroyEntries
 	{
 		internal override _IFsDisk _MakeTestSubject()
 		{
---
+++ Simulated.FileSystem.Tests/EncapsulateDifferencesBetweenDiskAndMemory/LocateExistingStructures.cs
@@ -0,0 +1,50 @@
+﻿// SimulatableAPI
+// File: LocateExistingStructures.cs
+// 
+// Copyright 2011, Arlo Belshee. All rights reserved. See LICENSE.txt for usage.
+
+using System;
+using System.Linq;
+using FluentAssertions;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Simulated.Tests.zzTestHelpers;
+using Simulated._Fs;
+
+namespace Simulated.Tests.EncapsulateDifferencesBetweenDiskAndMemory
+{
+	[TestFixture]
+	public abstract class LocateExistingStructures : DiskTestBase
+	{
+		[Test]
+		[TestCase("matches.*", new[] {"matches.txt", "matches.jpg"})]
+		[TestCase("*.*", new[] {"matches.txt", "matches.jpg", "no_match.txt"})]
+		[TestCase("*.txt", new[] {"matches.txt", "no_match.txt"})]
+		[TestCase("matches.txt", new[] {"matches.txt"})]
+		public void FileMatchingShouldMatchStarPatterns([NotNull] string searchPattern, [NotNull] string[] expectedMatches)
+		{
+			TestSubject.Overwrite(BaseFolder/"matches.txt", ArbitraryFileContents);
+			TestSubject.Overwrite(BaseFolder/"matches.jpg", ArbitraryFileContents);
+			TestSubject.Overwrite(BaseFolder/"no_match.txt", ArbitraryFileContents);
+			TestSubject.FindFiles(BaseFolder, searchPattern)
+				.Should()
+				.BeEquivalentTo(expectedMatches.Select(m => BaseFolder/m));
+		}
+	}
+
+	public class LocateExistingStructuresDiskFs : LocateExistingStructures
+	{
+		internal override _IFsDisk _MakeTestSubject()
+		{
+			return new _DiskReal();
+		}
+	}
+
+	public class LocateExistingStructuresMemoryFs : LocateExistingStructures
+	{
+		internal override _IFsDisk _MakeTestSubject()
+		{
+			return new _DiskSimulated();
+		}
+	}
+}
